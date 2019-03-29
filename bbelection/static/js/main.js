cd76fb6651b1a7720b86bad9904d1722284feef5
christinegithub <christine13@gmail.com>
2019-03-28 21:18:44 -0400

    exercise complete

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  const candidateList = document.getElementById('list');
+
+  axios.get('https://bb-election-api.herokuapp.com/')
+  .then((responseData) => {
+    responseData.data.candidates.forEach((candidate) => {
+      const individual = document.createElement('li');
+      individual.innerHTML = (`${candidate.name}: ${candidate.votes} votes`);
+      candidateList.appendChild(individual);
+    });
+  });
 });
